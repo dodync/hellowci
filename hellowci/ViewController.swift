43ef8e973d3b62e73b004852e09b540436591a6a
Dody <dodyrw@gmail.com>
2020-07-22 14:26:22 +0700

    Initial Commit

---
+++ hellowci/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hellowci
+//
+//  Created by Dody Wicaksono on 22/07/20.
+//  Copyright © 2020 Dody Wicaksono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
